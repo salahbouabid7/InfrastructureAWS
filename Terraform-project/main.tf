b60bdbaa17a5452579699319607d86127a0ba140
salahbouabid7 <112703676+salahbouabid7@users.noreply.github.com>
2025-05-19 01:30:52 +0100

    Update main.tf

--- Terraform-project/main.tf
+++ Terraform-project/main.tf
@@ -64,7 +64,8 @@ data "aws_internet_gateway" "default" {
 data "aws_instances" "instanceASG" {
   instance_tags = {
   Name = "ASG-instance" }
-  depends_on = [module.autoscaling]
+  depends_on = [null_resource.wait]
+  
 }
 
 # END #
@@ -130,11 +131,18 @@ module "autoscaling" {
 }
 # END #
 ## Parssing the private ip of the instance into inventory file of ansible ##
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 40 " 
+    ## The sleep 30 is used to wait for the instance to boot and receive a private IP
+  }
+  depends_on = [module.autoscaling]
+}
 resource "null_resource" "this" {
   provisioner "local-exec" {
     working_dir = "../automation_ansible"
-    command = "sleep 30 && sed -i \"s/TOBEREMPLACED/${data.aws_instances.instanceASG.private_ips[0]}\" ./instance-asg" ## The sleep 30 is used to wait for the instance to boot and receive a private IP
-}
+    command = " sed -i \"s/TOBEREMPLACED/${data.aws_instances.instanceASG.private_ips[0]}\" ./instance-asg" ## The sleep 30 is used to wait for the instance to boot and receive a private IP
+  }
   depends_on = [data.aws_instances.instanceASG]
 }
 
