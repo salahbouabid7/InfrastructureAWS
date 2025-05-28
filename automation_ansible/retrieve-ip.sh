8ab06cf0ae81b90ad7b92b507c570003054adf41
salah <salahbouabid7@gmail.com>
2025-05-28 17:02:31 +0100

    modification de ansible and buildspec

---
+++ automation_ansible/retrieve-ip.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+retrieve_ip(){
+    aws ec2 describe-instances                              \
+    --filters "Name=tag:Name,Values=ASG-instance"           \
+              "Name=instance-state-name,Values=running"     \
+    --query "Reservations[0].Instances[0].PrivateIpAddress" \
+    --output text 
+}
+echo "[*] Sleeping 120s to allow ASG to launch..."
+sleep 120
+
+while true;do
+ip=$(retrieve_ip)
+
+    if [ -z "${ip}" ];then 
+    echo "Instance not launched yet"
+    sleep 30
+    ip=$(retrieve_ip)
+
+    else
+    echo "[+] Private IP found: $ip"
+    break;
+    fi
+
+done
+
+echo "Private IP found: $ip"
+sed -i "s/TOBEREMPLACED/${ip}/g" ./automation_ansible/instance-asg
+
