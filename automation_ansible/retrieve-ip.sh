a23c4ba5cb99e80194e6e36480945a38ed7f64ce
salahbouabid7 <112703676+salahbouabid7@users.noreply.github.com>
2025-05-30 03:29:41 +0100

    Update retrieve-ip.sh

--- automation_ansible/retrieve-ip.sh
+++ automation_ansible/retrieve-ip.sh
@@ -1,30 +1,28 @@
-
 #!/bin/bash
-retrieve_ip(){
-    aws ec2 describe-instances                              \
-    --filters "Name=tag:Name,Values=ASG-instance"           \
-              "Name=instance-state-name,Values=running"     \
-    --query "Reservations[0].Instances[0].PrivateIpAddress" \
-    --output text 
+
+retrieve_ip() {
+    aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=ASG-instance" \
+                  "Name=instance-state-name,Values=running" \
+        --query "Reservations[].Instances[].PrivateIpAddress" \
+        --output text
 }
+
 echo "[*] Sleeping 120s to allow ASG to launch..."
 sleep 120
 
-while true;do
-ip=$(retrieve_ip)
-
-    if [ -z "${ip}" ];then 
-    echo "Instance not launched yet"
-    sleep 30
+while true; do
     ip=$(retrieve_ip)
 
+    if [[ -z "$ip" || "$ip" == "None" ]]; then
+        echo "[-] Instance not launched yet or no IP found."
+        sleep 30
     else
-    echo "[+] Private IP found: $ip"
-    break;
+        echo "[+] Private IP found: $ip"
+        break
     fi
-
 done
 
 echo "Private IP found: $ip"
-sed -i "s/TOBEREMPLACED/${ip}/g" ./instance-asg
 
+sed -i "s/TOBEREMPLACED/${ip}/g" ./instance-asg
